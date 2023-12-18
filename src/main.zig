3d8f35f29058ab25ed721effb7a838b04b5f2dd1
Michael Scheffenacker <michael@gmail.com>
2023-12-18 23:36:11 +0100

    add fraction reduction

--- src/main.zig
+++ src/main.zig
@@ -1,52 +1,73 @@
 const std = @import("std");
 const print = std.debug.print;
 
-const Frac = struct { num: u64, den: u64 };
+const Frac = struct { num:u64, den:u64 };
 
+const maxInt = std.math.maxInt(u64);
 var primes: [1024]u64 = undefined;
 
+var currentFactorization:u64 = 0;
 var primeFactorizations: [128][128]u64 = undefined;
 
 const PrimeFacErr = error { miss };
 
+const Lcm = struct { fac1:u64, fac2:u64 };
+
 pub fn main() !void {
     generatePrimes();
-
     
-    
-    const a = Frac{ .num = 5, .den = 12 };
+    const a = Frac{ .num = 5, .den = 8 };
     const b = Frac{ .num = 3, .den = 12 };
-    const c = add(a, b);
+    const factorsA = try primeFactorize(a.den);
+    const factorsB = try primeFactorize(b.den);
+    const lcm:Lcm = calcLcm(factorsA, factorsB);
+    const aNorm = normalize(a, lcm.fac1);
+    const bNorm = normalize(b, lcm.fac2);
+    
+    const c = add(aNorm, bNorm);
+    const cReduced = try reduceFrac(c);
 
-    print("\n", .{});
     print("\n", .{});
 
     printFrac(a);
     print(" + ", .{});
     printFrac(b);
     print(" = ", .{});
+    printFrac(aNorm);
+    print(" + ", .{});
+    printFrac(bNorm);
+    print(" = ", .{});
+    
     printFrac(c);
+    print(" = ", .{});
+    printFrac(cReduced);
 
     print("\n", .{});
-
-    const number1 = 156;
-    _ = try primeFactorize(number1);
-    const number2 = 133;
-    _ = try primeFactorize(number2);
 
     
 }
 
-fn add(a: Frac, b: Frac) Frac {
+fn add(a:Frac, b:Frac) Frac {
     const c = Frac{ .num = a.num + b.num, .den = a.den };
     return c;
+}
+
+fn normalize(a:Frac, lcmFac:u64) Frac {
+    return Frac{ .num = a.num * lcmFac, .den = a.den * lcmFac };
+}
+
+fn reduceFrac(a:Frac) !Frac {
+    const factorsNum = try primeFactorize(a.num);
+    const factorsDen = try primeFactorize(a.den);
+    const gcd:u64 = calcGcd(factorsNum, factorsDen);
+    return Frac{ .num = a.num / gcd, .den = a.den / gcd };
 }
 
 fn printFrac(a: Frac) void {
     std.debug.print("{}/{}", .{a.num, a.den});
 }
 
-fn printFac(number: u64, factors: []u64) void {
+fn printFac(number:u64, factors:[]u64) void {
     print("{} = ", .{number});
     for (factors) |p| { print( "{}·", .{p}); }
     print("\n", .{});
@@ -55,10 +76,10 @@ fn printFac(number: u64, factors: []u64) void {
 
 fn generatePrimes() void {
     primes[0] = 2;
-    var i: u64 = 3;
-    var p: u64 = 1;
+    var i:u64 = 3;
+    var p:u64 = 1;
     while (p < primes.len) : (i += 1) {
-        var q: u64 = 0;
+        var q:u64 = 0;
         while (q < p) : (q += 1) {
             if (i % primes[q] == 0)  { break; }
         }
@@ -71,10 +92,12 @@ fn generatePrimes() void {
     for (primes[0..8]) |prime| { print("{} ", .{prime}); }
     print("... ", .{});
     for (primes[1018..]) |prime| { print("{} ", .{prime}); }
+    print("\n", .{});
 }
 
 fn primeFactorize(number: u64) ![]u64 {
-    var facs:[]u64 = primeFactorizations[0][0..];
+    var facs:[]u64 = primeFactorizations[currentFactorization][0..];
+    currentFactorization += 1;
     var n:u64 = number;
     for (facs, 0..) |*fac, i| {
         for (primes) |prime| {
@@ -95,6 +118,55 @@ fn primeFactorize(number: u64) ![]u64 {
     return PrimeFacErr.miss;
 }
 
+fn calcLcm(factors1:[]u64, factors2:[]u64) Lcm {
+    var lcm:Lcm = .{.fac1 = 1, .fac2 = 1};
+    var index1:u64 = 0;
+    var index2:u64 = 0;
+    while (true) {
+        const val1:u64 = if (index1 < factors1.len) factors1[index1] else maxInt;
+        const val2:u64 = if (index2 < factors2.len) factors2[index2] else maxInt;
+
+        print("{} {}\n", .{lcm.fac1, lcm.fac2});
+        
+        if (val1 == maxInt and val2 == maxInt) {  return lcm; }
+                
+        if (val1 == val2) {
+            index1 += 1;
+            index2 += 1;
+        } else if (val1 < val2) {
+            lcm.fac2 *= val1;
+            index1 += 1;
+        } else {
+            lcm.fac1 *= val2;
+            index2 += 1;
+        }
+    }
+    
+}
+
+fn calcGcd(factors1:[]u64, factors2:[]u64) u64 {
+    var gcd:u64 = 1;
+    var index1:u64 = 0;
+    var index2:u64 = 0;
+    while (true) {
+        const val1:u64 = if (index1 < factors1.len) factors1[index1] else maxInt;
+        const val2:u64 = if (index2 < factors2.len) factors2[index2] else maxInt;
+
+        print("{} {} {} {} {} \n", .{index1, index2, val1, val2, gcd});
+
+        if (val1 == maxInt and val2 == maxInt) {  return gcd; }
+
+        if (val1 == val2) {
+            index1 += 1;
+            index2 += 1;
+            gcd *= val1;
+        } else if (val1 < val2) {
+            index1 += 1;
+        } else {
+            index2 += 1;
+        }
+    }
+}
 
 // test "simple test" {
 //     var list = std.ArrayList(i32).init(std.testing.allocator);
